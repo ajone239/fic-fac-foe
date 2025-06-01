2658bf4edcb67352475d214670eff343d36766d4
Austin Jones <ajones53.aj@gmail.com>
2025-06-01 12:59:54 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<h1>Fic Fac Foe!</h1>
+
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+	<a href="/how_to_play">how to play</a>
+</nav>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Cell from './Cell.svelte';
+
+	let game_state = $state(<number[]>[]);
+
+	let counts = [...Array(4).keys()];
+</script>
+
+<h1>the place the game will go</h1>
+
+<div class="game-board">
+	{#each counts as row (row)}
+		<div class="game-row">
+			{#each counts as col (col)}
+				<Cell id={row * 4 + col} {game_state} />
+			{/each}
+		</div>
+	{/each}
+</div>
+
+<style>
+	.game-row {
+		display: flex;
+		flex-direction: row;
+		gap: 0.5em;
+	}
+	.game-board {
+		display: flex;
+		gap: 0.5em;
+		flex-direction: column;
+		align-items: center; /* center rows horizontally */
+		justify-content: center; /* center vertically */
+	}
+</style>
